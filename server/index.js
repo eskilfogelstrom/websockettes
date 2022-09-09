e5b3c3b5b3b659c075fd98eafae8034809627f98
Eskil Fogelström <eskil@epidemicsound.com>
2022-09-09 14:39:22 +0200

    Port to env var

--- server/index.js
+++ server/index.js
@@ -53,6 +53,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3000, () => {
-  console.log('Listening on 3000');
+server.listen(process.env.PORT, () => {
+  console.log('Listening on ', process.env.PORT);
 });

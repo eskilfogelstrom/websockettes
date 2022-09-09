8015aee51b24fbf7d3f61e7c4485f4457e04e8d6
Eskil Fogelström <eskil@epidemicsound.com>
2022-09-09 14:36:33 +0200

    Allow all

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(() => {
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:1234",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });

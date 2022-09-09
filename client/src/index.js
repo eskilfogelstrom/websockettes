5033cc11a8032ff1d5fd8fdba6e43f1f5a234593
Eskil Fogelström <eskil@epidemicsound.com>
2022-09-09 10:39:40 +0200

    Add connect button

--- client/src/index.js
+++ client/src/index.js
@@ -2,42 +2,57 @@ import { io } from 'socket.io-client';
 import kickUrl from 'url:./samples/kick.wav';
 import snareUrl from 'url:./samples/snare.wav';
 
-const socket = io('ws://longing-island-leech.glitch.me/');
 
-socket.on('setInstrument', msg => {
-    console.log(msg);
-})
 
-const buttonTrigger = document.getElementById('buttonTrigger');
+const buttonConnect = document.getElementById('buttonConnect');
+buttonConnect.onclick = () => {
+    const socket = connect();
 
-buttonTrigger.onclick = e => {
-    socket.emit('trigger');
+    const buttonTrigger = document.createElement('button');
+    buttonTrigger.innerText = 'Trigger';
+    buttonTrigger.onclick = e => {
+        socket.emit('trigger');
+    };
+    buttonConnect.remove();
+    document.body.appendChild(buttonTrigger);
 }
 
 
-const context = new AudioContext();
+const connect = () => {
+    const socket = io('ws://longing-island-leech.glitch.me/');
 
-const instrumentUrls = {
-    kick: kickUrl,
-    snare: snareUrl
-};
-const instruments = Object.keys(instrumentUrls);
+    socket.on('setInstrument', msg => {
+        console.log(msg);
+    })
 
+    const context = new AudioContext();
 
-const instrumentNodes = instruments.reduce((nodes, instrument) => {
-    const url = instrumentUrls[instrument];
-    const el = new Audio(url);
-    const sourceNode = context.createMediaElementSource(el);
-    sourceNode.connect(context.destination);
-
-    return {
-        ...nodes,
-        [instrument]: el
+    const instrumentUrls = {
+        kick: kickUrl,
+        snare: snareUrl
     };
-}, {});
+    const instruments = Object.keys(instrumentUrls);
 
 
-socket.on('trigger', msg => {
-    const node = instrumentNodes[msg.instrument];
-    node.play();
-});
+    const instrumentNodes = instruments.reduce((nodes, instrument) => {
+        const url = instrumentUrls[instrument];
+        const el = new Audio(url);
+        const sourceNode = context.createMediaElementSource(el);
+        sourceNode.connect(context.destination);
+
+        return {
+            ...nodes,
+            [instrument]: el
+        };
+    }, {});
+
+
+    socket.on('trigger', msg => {
+        console.log('trigger', msg);
+        const node = instrumentNodes[msg.instrument];
+        node.currentTime = 0;
+        node.play();
+    });
+
+    return socket;
+}
